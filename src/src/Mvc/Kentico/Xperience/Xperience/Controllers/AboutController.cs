1db23aa09735b4bd66d08f9e87c57df4cc2e7f79
Samuel Steele <ssteele@bizstream.com>
2021-05-05 16:42:34 -0400

    correct csproj property name; added Xperience controller

--- src/src/Mvc/Kentico/Xperience/Xperience/Controllers/AboutController.cs
+++ src/src/Mvc/Kentico/Xperience/Xperience/Controllers/AboutController.cs
@@ -1,38 +1,16 @@
-using AutoMapper;
 using BlogTemplate.Infrastructure.Kentico.Xperience.Abstractions.PageTypes;
+using BlogTemplate.Mvc.Kentico.Xperience.Abstractions;
 using BlogTemplate.Mvc.Kentico.Xperience.Models;
-using Kentico.Content.Web.Mvc;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogTemplate.Mvc.Kentico.Xperience.Controllers
 {
 
-    public class AboutController : Controller
+    public class AboutController : XperienceController
     {
-        #region Fields
-        private readonly IMapper mapper;
-        private readonly IPageDataContextRetriever retriever;
-        #endregion
-
-        public AboutController(
-            IMapper mapper,
-            IPageDataContextRetriever retriever
-        )
-        {
-            this.mapper = mapper;
-            this.retriever = retriever;
-        }
 
         public IActionResult Index( )
-        {
-            if( !retriever.TryRetrieve<AboutNode>( out IPageDataContext<AboutNode> data ) )
-            {
-                return NotFound();
-            }
-
-            var viewModel = mapper.Map<AboutViewModel>( data.Page );
-            return View( viewModel );
-        }
+            => PageView<AboutNode, AboutViewModel>();
 
     }
 
